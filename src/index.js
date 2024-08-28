44eadda2a7044430bc15dc3ade37d80c58a0182c
Tanja Kiseleva <lessy_92@mail.ru>
2024-08-28 21:50:32 +0200

    fix: fix the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+        let numToStr = n + "";
+        let strToArr = numToStr.split('').map(Number).reverse();
+        let arrToStr = strToArr.join('');
+        let strToNum = parseInt(arrToStr);
+        return strToNum;
 }
